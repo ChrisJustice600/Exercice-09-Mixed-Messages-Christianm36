ed12ca8d2cf5533582bf280dce55da59bf63bdd3
ChrisJustice600 <85912956+ChrisJustice600@users.noreply.github.com>
2023-07-27 10:15:17 +0000

    add return function
    add return function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // fugd
+
+function returnMessage () {
+   const messages = [
+    "bonjour",
+    "hello",
+    "tres gentille",
+    "justice",
+    "buzz"
+   ];
+
+   return messages[Math.floor(Math.random() * messages.length)];
+};
+
+const message = returnMessage();
+console.log(message)
