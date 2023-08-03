d8f262b0f28a3aed038e8a744173075ba29ded80
Mabeladi <christian.mabeladi@kadea.local>
2023-08-03 11:40:38 +0100

    last commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Commentair
+function returnMessage () {
+   const messages = [
+    "bonjour",
+    "hello",
+    "tres gentille",
+    "justice",
+    "buzz",
+    "elon musk",
+    "sorry",
+    "lajoie"
+   ];
+
+   return messages[Math.floor(Math.random() * messages.length)];
+};
+
+console.log("hello man")
+
+// console
+const message = returnMessage();
+console.log(message)
